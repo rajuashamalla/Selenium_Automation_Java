7b4ea9392483d63ba8c924a6e0b113f684bfec87
rajuashamalla <ashamallaraju28340@gmail.com>
2025-08-05 15:33:22 +0530

    Initial Selenium Maven Project setup

---
+++ src/test/java/day01_BrowserSetup_And_Locators/FirstTestCase.java
@@ -0,0 +1,42 @@
+package day01_BrowserSetup_And_Locators;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ * 1) Launch browser (chrome)
+ * 2) open url https://www.google.com/
+ * 3) validate title should be"Google"
+ * 4) close browser
+ */
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+		
+		 // 1) Launch browser (chrome)
+	//	ChromeDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
+		
+		// 2) open url https://www.google.com/
+			driver.get("https://www.google.com/");
+		
+		//3) validate title should be "Google"
+			String act_title=driver.getTitle();
+			if(act_title.equals("Google"))
+			{
+				System.out.println("Test Passed....");
+			}
+			else
+			{
+				System.out.println("Test Failed....");
+			}
+			//4) close browser
+			//driver.close();
+			driver.quit();
+			
+			
+		
+	}
+
+}
